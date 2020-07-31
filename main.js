4e9f41eef4cdf99848f697079c75bf56131420bf
ryomakawakami <ryomakwkm@gmail.com>
2020-07-31 18:00:28 -0400

    Ping

--- main.js
+++ main.js
@@ -5,8 +5,21 @@ if (process.env.NODE_ENV !== 'production') {
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = '!'
+
 client.once('ready', () => {
     console.log('Online!')
+})
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase()
+
+    if (command == 'ping') {
+        message.channel.send('pong')
+    }
 })
 
 token = process.env.token
